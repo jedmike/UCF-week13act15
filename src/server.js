90887c8c4a3c2b924fbed4e50e7ea9cea00b6f87
Dana Cronin <cronin43224235@gmail.com>
2019-12-11 19:49:39 -0500

    psuedocodeing

--- src/server.js
+++ src/server.js
@@ -11,3 +11,41 @@ app.use(express.static(publicDirectoryPath));
 app.listen(port, () => {
     console.log(`Server is up on port: ${port}`)
 })
+
+// =========== tables on load
+// retrieve data (get)
+// append [0 - 4] to tables
+// append [5 - ... ] to waitlist
+
+// Displays all characters
+// app.get("/api/partiess", function(req, res) {
+//     return res.json(parties);
+//   });
+
+// Displays a single character, or shows "No character found"
+// app.get("/api/parties/:party", function(req, res) {
+//     for (var i = 0; i < parties.length; i++) {
+//       if (i < 5) {
+        // console.log(`table: ${i + 1}`);
+//         return res.json(parties[i]);
+//       } else {
+    // console.log(`waitlist: ${i + 1}`);
+            //append to waitlist
+// }
+//     }
+//     return res.send("No party found");
+//   });
+
+//++++++++++++++++++++++++++++++++
+
+// ====== reserve page on submit-btn click
+// retrieve data
+// send to storage space (post)
+
+// Create New Characters - takes in JSON input
+// app.post("/api/parties", function(req, res) {
+//     var newParty = req.body;
+//     console.log(newParty);
+//     parties.push(newParty);
+//     res.json(newParty);
+// });
